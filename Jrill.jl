8066bbfd46e3c4975489a2ba53c690ab13295914
ssikdar1 <shan.sikdar@gmail.com>
2019-05-14 23:32:37 -0400

    add struct for conn info

--- Jrill.jl
+++ Jrill.jl
@@ -1,7 +1,9 @@
 
 using HTTP
 using BufferedStreams
+using Parameters
 import JSON
+
 
 # TODO make a connection class
 # TODO organize repo into a module structure
@@ -12,13 +14,36 @@ import JSON
 # TODO results to Dataframe
 # TODO is this slow?
 
-function drill_options()
-    HTTP.open("GET", "http://192.168.0.20:8047/options.json") do http
+@with_kw struct DrillConnection
+   host::String
+   port::Int
+   ssl::Bool = false
+end
+
+
+function make_url(conn::DrillConnection)
+    proto = conn.ssl ? "https" : "http"
+    return "$proto://$(conn.host):$(conn.port)"
+end
+
+function drill_options(conn::DrillConnection)
+    url = make_url(conn)
+    HTTP.open("GET", "$url/options.json") do http
        io = BufferedInputStream(http)
        println(JSON.parse(io))
     end	
 end
 
 
+function drill_connection(host::String, port::Integer)
+    return DrillConnection(host=host,port=port)
+end
+
 println("hello world")
-drill_options()
+
+drill_conn = drill_connection("localhost", 8047) 
+println(drill_conn)
+println(make_url(drill_conn))
+drill_options(drill_conn)
+# TODO timeout
+"http://192.168.0.20:8047/options.json"
