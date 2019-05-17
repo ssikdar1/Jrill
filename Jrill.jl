11a41d0f5b20b0aba8486bfbe773fe821b791757
ssikdar-r7 <shan.sikdar@gmail.com>
2019-05-17 16:29:39 -0400

    flesh out documentation

--- Jrill.jl
+++ Jrill.jl
@@ -1,9 +1,10 @@
 
 using HTTP
 using BufferedStreams
+using DataFrames
 import JSON
 
-
+#TODO jdbc
 # TODO organize repo into a module structure
 # with different functions
 # TODO HTTPS
@@ -21,6 +22,12 @@ end
 	make_url(conn::DrillConnection)
 
 Create base url for connection to Drill REST API
+
+# Arguments
+- `conn::DrillConnection`: Drill Connection info
+
+# Returns
+- base url string for HTTP requests
 """
 function make_url(conn::DrillConnection)
     proto = conn.ssl ? "https" : "http"
@@ -28,9 +35,54 @@ function make_url(conn::DrillConnection)
 end
 
 """
+   drill_query(conn::DrillConnection, query::String; to_df::Bool=false, limit::Int=nothing)
+
+Submit a query and return results. 
+
+# Arguments
+- `conn::DrillConnection`: Drill Connection info
+- `query::String`: query to submit
+- `to_df::Bool=false`: to return as Dataframe otherwise return JSON.parse of the result
+- `limit::Int=nothing`: sets autoLimit in the HTTP request to limit number of rows (Drill 1.16+)
+
+# Returns
+- Dataframe or the result of JSON.parse depending on to_df
+"""
+function drill_query(conn::DrillConnection, query::String; to_df::Bool=false, limit::Int=-1)
+    url = make_url(conn)
+    io = Base.BufferStream()
+    headers = ["Content-Type" => "application/json"]
+    params = Dict(
+                  "queryType"=>"SQL",
+                  "query" => query
+                 )
+    if limit != -1
+       params["autoLimit"] = limit
+    end
+
+    r = HTTP.request("POST", "$url/query.json", 
+             headers,
+             JSON.json(params),
+             response_stream=io)
+    # TODO create Dataframe
+    if to_df
+       println(JSON.parse(io))
+    else
+      return JSON.parse(io)
+    end
+end
+
+
+"""
 	drill_cluster(conn::DrillConnection)	
 	
 Get Drillbit information, such as port numbers.
+
+# Arguments
+- `conn::DrillConnection`: Drill Connection info
+
+# Returns
+- the result of JSON.parse
 
 """
 function drill_cluster(conn::DrillConnection)
@@ -46,13 +98,19 @@ end
 
 TODO
 """
-function drill_status(conn::DrillConnection)
-end
+#function drill_status(conn::DrillConnection)
+#end
 
 """
 	drill_metrics(conn::DrillConnection)
 
 Get the current memory metrics.
+
+# Arguments
+- `conn::DrillConnection`: Drill Connection info
+
+# Returns
+- the result of JSON.parse
 
 """
 function drill_metrics(conn::DrillConnection)
@@ -67,7 +125,11 @@ end
 
 Get the status of threads.
 
-Return String
+# Arguments
+- `conn::DrillConnection`: Drill Connection info
+
+# Returns
+- String of the request body
 """
 function drill_threads(conn::DrillConnection)
     url = make_url(conn)
@@ -86,8 +148,11 @@ List the name, default, and data type of the system and session options.
 julia> dc = drill_connection("localhost", 8047) 
 julia> drill_options(drill_conn) 
 ```
+# Arguments
+- `conn::DrillConnection`: Drill Connection info
 
-Return Any[Dict{String,Any}, ...] 
+# Returns
+- the result of JSON.parse
 """
 function drill_options(conn::DrillConnection)
     url = make_url(conn)
@@ -104,12 +169,15 @@ end
 # Test block #
 println("hello world")
 
-drill_conn = drill_connection("localhost", 8047) 
+drill_conn = drill_connection("192.168.0.20", 8047) 
 println(drill_conn)
 println(make_url(drill_conn))
-r = drill_options(drill_conn)
+#r = drill_options(drill_conn)
 #println(typeof(r))
 #println(drill_threads(drill_conn))
-println(drill_metrics(drill_conn))
+#println(drill_metrics(drill_conn))
 # TODO timeout
-"http://192.168.0.20:8047/options.json"
+#"http://192.168.0.20:8047/options.json"
+query = "SELECT * FROM sys.version"
+ret = drill_query(drill_conn, query)
+println(ret)
